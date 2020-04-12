4aec03b7913d05b329c9b006c265aaecb3d5aebf
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-04-12 23:02:39 +0900

    Add dynamic duration time for transition

--- components/Elements/FadeTransition.vue
+++ components/Elements/FadeTransition.vue
@@ -2,8 +2,8 @@
   <transition
     name="fade"
     v-bind="$attrs"
-    enter-active-class="transition duration-100 ease-out"
-    leave-active-class="transition duration-75 ease-in"
+    :enter-active-class="`transition ease-out duration-${enter}`"
+    :leave-active-class="`transition ease-in duration-${leave}`"
     enter-class="transform scale-95 opacity-0"
     leave-class="transform scale-100 opacity-100"
     enter-to-class="transform scale-100 opacity-100"
@@ -16,6 +16,40 @@
 
 <script>
 export default {
-  inheritAttrs: false
+  inheritAttrs: false,
+  props: {
+    enter: {
+      validator(value) {
+        // The value must match one of these strings
+        return [
+          '75',
+          '100',
+          '150',
+          '200',
+          '300',
+          '500',
+          '700',
+          '1000'
+        ].includes(value)
+      },
+      default: '100'
+    },
+    leave: {
+      validator(value) {
+        // The value must match one of these strings
+        return [
+          '75',
+          '100',
+          '150',
+          '200',
+          '300',
+          '500',
+          '700',
+          '1000'
+        ].includes(value)
+      },
+      default: '75'
+    }
+  }
 }
 </script>
