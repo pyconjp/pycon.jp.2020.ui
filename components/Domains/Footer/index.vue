d6c75abf87022c1a281507bb88d181546ae816e0
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-04-14 14:46:45 +0900

    Beutify footer margin

--- components/Domains/Footer/index.vue
+++ components/Domains/Footer/index.vue
@@ -1,15 +1,26 @@
 <template>
-  <div class="flex flex-col items-center justify-around h-24 my-8 md:flex-row">
-    <h2>
+  <div
+    class="flex flex-col items-center justify-between max-w-screen-xl px-8 my-8 md:items-center md:flex-row"
+  >
+    <h2
+      class="mb-8 text-xl font-semibold tracking-widest text-gray-900 uppercase md:mb-0"
+    >
       <n-link :to="localePath('/')">
         Pycon JP 2020
       </n-link>
     </h2>
-    <div class="flex flex-col items-start">
-      <h3>主催: 一般社団法人PyCon JP</h3>
+    <div
+      class="flex flex-col items-start justify-end justify-between md:ml-auto"
+    >
+      <h3 class="mb-2">主催: 一般社団法人PyCon JP</h3>
       <h3>
         PyCon JP 2020 in Tokyo is a production of the
-        <a href="https://www.pycon.jp/">
+        <a
+          class="text-blue-400 transition-colors duration-100 hover:text-blue-600"
+          href="https://www.pycon.jp/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
           PyCon JP Committee
         </a>
       </h3>
