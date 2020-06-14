2d2819a1aad7eb1ff7194d8bfc9cca844e32da6b
Hiroya Akita <akky.develop@gmail.com>
2020-06-14 17:08:17 +0900

    fix: update header component

--- components/Domains/Header/LocalesList.vue
+++ components/Domains/Header/LocalesList.vue
@@ -3,7 +3,7 @@
     <n-link
       v-for="locale in availableLocales"
       :key="locale.code"
-      class="flex items-center px-4 py-2 mt-2 transition-colors duration-200 bg-transparent rounded-lg px- md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+      class="flex items-center px-4 py-2 mt-2 transition-colors duration-200 bg-transparent border-2 rounded-lg border-py-black px- md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
       :to="switchLocalePath(locale.code)"
     >
       <i class="mr-2 text-gray-700 material-icons">translate</i>
