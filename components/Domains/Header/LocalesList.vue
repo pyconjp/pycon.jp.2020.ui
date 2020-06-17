8d0eaaac01c2df1e21f62ae3623f1d092f24b616
Koji Ishimoto <koji.ishimoto@gmail.com>
2020-06-17 15:13:41 +0900

    add transition and margin

--- components/Domains/Header/LocalesList.vue
+++ components/Domains/Header/LocalesList.vue
@@ -3,11 +3,11 @@
     <n-link
       v-for="locale in availableLocales"
       :key="locale.code"
-      class="flex items-center px-4 py-2 mt-2 transition-colors duration-200 bg-transparent border-2 rounded-lg border-py-black px- md:mt-0 hover:text-gray-900 focus:text-gray-900 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
+      class="flex items-center px-4 py-2 mt-2 transition-colors duration-200 bg-transparent border-2 rounded-lg border-py-black md:mt-0 hover:bg-py-black group"
       :to="switchLocalePath(locale.code)"
     >
-      <i class="mr-2 text-gray-700 material-icons">translate</i>
-      {{ locale.name }}
+      <i class="mr-2 material-icons group-hover:text-white">translate</i>
+      <span class="group-hover:text-white">{{ locale.name }}</span>
     </n-link>
   </div>
 </template>
