058e6c9085ff8882c6f10d02d7c8ce1a379397ad
matsumana07384 <manami.m.227@outlook.jp>
2020-07-23 12:20:32 +0900

    add dom block

---
+++ pages/tutorial.vue
@@ -0,0 +1,35 @@
+<template>
+  <section
+    class="relative flex items-start justify-center mb-12 h-80 md:h-120 md:items-center"
+  >
+    <div class="hero-block"></div>
+    <div class="way-to-join-block"></div>
+    <div class="description-block">
+      <div class="event-one"></div>
+      <div class="event-two"></div>
+    </div>
+  </section>
+</template>
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  components: {},
+})
+</script>
+<style scoped>
+.overview-area {
+  background-color: #ffd8a1;
+}
+</style>
+
+<i18n>
+{
+    "en": {
+
+    },
+    "ja": {
+
+    }
+}
+</i18n>
