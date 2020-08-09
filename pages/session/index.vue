9dd53fab246c8b3954228a3c236de12f1dfc9466
Hiroya Akita <akky.develop@gmail.com>
2020-08-10 02:28:00 +0900

    feat: add sessionize timetable implementation

--- pages/session/index.vue
+++ pages/session/index.vue
@@ -1,6 +1,11 @@
 <template>
   <div>
-    {{ sessions }}
+    <iframe
+      id="iframeid"
+      src="https://dev7367.d2hce9hbdo38af.amplifyapp.com/"
+      frameborder="0"
+      class="w-full frame"
+    ></iframe>
   </div>
 </template>
 <script>
@@ -9,7 +14,17 @@ export default {
     const sessions = require('~/mocks/session.json')
     return { sessions }
   },
+  mounted() {
+    window.addEventListener(
+      'message',
+      function (element) {
+        const iframe = document.getElementById('iframeid')
+        iframe.style.height = element.data + 'px'
+      },
+      false
+    )
+  },
 }
 </script>
 
-<style></style>
+<style scoped></style>
---
+++ sessionize/README.md
@@ -0,0 +1,8 @@
+## 概要
+
+sessionizeのタイムテーブルをiframeで読み込んでおり、その元サイトのHTML  
+ホスティングはAWS Amplifyで行っており、デプロイする場合はindex.htmlをzip圧縮してドラッグ&ドロップでデプロイする
+
+```
+$ cd sessionize && zip upload index.html && cd -
+```
