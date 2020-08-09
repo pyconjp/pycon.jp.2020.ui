6f58dc006fa193f29664f360e5cb62c2cef0e857
ftnext <takuyafjp+develop@gmail.com>
2020-08-10 01:04:51 +0900

    ✨complete entrypoint (enable to fetch from sessionize APIs)

--- tools/main.py
+++ tools/main.py
@@ -1,31 +1,32 @@
 import argparse
 import csv
 
-from sessionfetcher import http
+from sessionfetcher import fetch_talks_as_dict
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("sessionize_uri")
-    parser.add_argument("-d", "--dest", default="sessions.csv")
+    parser.add_argument("sessions_uri")
+    parser.add_argument("speakers_uri")
+    parser.add_argument("-d", "--dest", default="talks.csv")
     return parser.parse_args()
 
 
-def to_csv(sessions, dest_path):
-    fields = sessions.fields
+def to_csv(talks, dest_path, fields):
     with open(dest_path, "w") as f:
         writer = csv.DictWriter(f, fieldnames=fields)
         writer.writeheader()
-        for session in sessions:
-            writer.writerow(session)
+        writer.writerows(talks)
 
 
 def main():
     args = parse_args()
-    # sessionsを取得 引数 args.sessionize_uri
-    # - sessionizeのAPIにリクエスト（返り値を取得）
-    # - 返り値のパース
-    to_csv(sessions, args.dest)
+
+    talks = fetch_talks_as_dict(args.sessions_uri, args.speakers_uri)
+    talks_list = list(talks)
+    fields = list(talks_list[0].keys())
+
+    to_csv(talks_list, args.dest, fields)
 
 
 if __name__ == "__main__":
