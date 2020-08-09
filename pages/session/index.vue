0616106544d760c93178f59ae4945f09606e7138
Hiroya Akita <akky.develop@gmail.com>
2020-08-10 02:35:55 +0900

    fix: add title section in timetable page

--- pages/session/index.vue
+++ pages/session/index.vue
@@ -1,5 +1,17 @@
 <template>
   <div>
+    <div class="flex flex-col items-center justify-center h-32 md:h-56">
+      <h1
+        class="flex items-center justify-center w-screen text-2xl font-semibold text-center text-py-black sm:text-3xl md:text-4xl lg:text-5xl m-screen"
+      >
+        Sponsors
+      </h1>
+      <h3
+        class="flex items-center justify-center w-screen mb-8 text-lg font-semibold text-center text-py-black sm:text-xl md:text-2xl lg:text-3xl m-screen"
+      >
+        {{ $t('pages["event-list"]["content-list"][0].title') }}
+      </h3>
+    </div>
     <iframe
       id="iframeid"
       src="https://dev7367.d2hce9hbdo38af.amplifyapp.com/"
