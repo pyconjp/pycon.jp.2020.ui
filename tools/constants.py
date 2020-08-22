af78bb6db14b63e85b117dd7e06fae534c91790a
ftnext <takuyafjp+develop@gmail.com>
2020-08-22 14:24:05 +0900

    🔧 add constants for streaming

---
+++ tools/constants.py
@@ -0,0 +1,20 @@
+YOUTUBE_LIVE_URL_MAP = {
+    # day, room -> URL
+    (1, "#pyconjp"): "https://youtu.be/rq1LHsvJmWA",
+    (1, "#pyconjp_1"): "https://youtu.be/taVDsm4bqC8",
+    (1, "#pyconjp_2"): "https://youtu.be/4ijaTU_PhN8",
+    (1, "#pyconjp_3"): "https://youtu.be/wHvhnwOyvIg",
+    (1, "#pyconjp_4"): "https://youtu.be/HYJxJEQyGpk",
+    (1, "#pyconjp_5"): "https://youtu.be/dXEsGUed0fo",
+    (2, "#pyconjp"): "https://youtu.be/-ewM4Wz50wA",
+    (2, "#pyconjp_1"): "https://youtu.be/7HCJD4oxfuo",
+    (2, "#pyconjp_2"): "https://youtu.be/qbsJY3M7eIo",
+    (2, "#pyconjp_3"): "https://youtu.be/-IxP81W4D24",
+    (2, "#pyconjp_4"): "https://youtu.be/1hw6AIzqehk",
+    (2, "#pyconjp_5"): "https://youtu.be/krnaOxKRhoQ",
+}
+
+# sessionizeのタイムテーブルに入っているコンテンツのうち配信しないもの
+NOT_STREAMED_ID_SET = set(
+    ["215380", "215531"]  # PyCon JP Association 運営会議(公開型), Online Party
+)
