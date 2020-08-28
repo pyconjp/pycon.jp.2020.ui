83a0f88bc746001231adfaa894dc8a7924183bef
Hiroya Akita <akky.develop@gmail.com>
2020-08-28 16:59:02 +0900

    fix: add youtube live link page on header

--- components/Domains/Header/EventList.vue
+++ components/Domains/Header/EventList.vue
@@ -3,15 +3,27 @@
     class="absolute right-0 w-full mt-2 origin-top-right rounded-md shadow-lg md:w-48"
   >
     <div class="px-2 py-2 bg-white rounded-md shadow">
-      <n-link
+      <div
         v-for="event in $t('pages.event-list.content-list')"
         :key="event.title"
-        class="block px-4 py-2 mt-2 text-sm font-semibold transition-colors duration-200 bg-transparent rounded-lg px- md:mt-0 focus:text-gray-900 hover:text-gray-600 focus:bg-gray-200 focus:outline-none"
-        :to="localePath(event.path)"
-        @click.native.stop="$emit(emitEvent)"
       >
-        {{ event.title }}
-      </n-link>
+        <n-link
+          v-if="judgeNuxtPath(event.path)"
+          class="block px-4 py-2 mt-2 text-sm font-semibold transition-colors duration-200 bg-transparent rounded-lg px- md:mt-0 focus:text-gray-900 hover:text-gray-600 focus:bg-gray-200 focus:outline-none"
+          :to="localePath(event.path)"
+          @click.native.stop="$emit(emitEvent)"
+        >
+          {{ event.title }}
+        </n-link>
+        <a
+          v-else
+          :href="event.path"
+          class="block px-4 py-2 mt-2 text-sm font-semibold transition-colors duration-200 bg-transparent rounded-lg px- md:mt-0 focus:text-gray-900 hover:text-gray-600 focus:bg-gray-200 focus:outline-none"
+          target="_blank"
+          rel="noopener noreferrer"
+          >{{ event.title }}</a
+        >
+      </div>
     </div>
   </div>
 </template>
@@ -25,5 +37,10 @@ export default {
     },
   },
   computed: {},
+  methods: {
+    judgeNuxtPath(path) {
+      return !/^https?:\/\//.test(path)
+    },
+  },
 }
 </script>
